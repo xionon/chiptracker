4d9a7ba096bfb20d2dd39d98956b552f87110866
Alec Hipshear <alechipshear@gmail.com>
2016-02-18 20:46:54 -0500

    Configure application

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,19 @@ Bundler.require(*Rails.groups)
 
 module Chiptracker
   class Application < Rails::Application
+    config.generators do |generate|
+      generate.helper false
+      generate.javascript_engine false
+      generate.stylesheets false
+
+      generate.test_framework :rspec
+      generate.request_specs false
+      generate.routing_specs false
+      generate.view_specs false
+    end
+
+    config.log_tags = [ :uuid ]
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
